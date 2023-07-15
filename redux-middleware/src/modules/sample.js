ee0b4a284ef738e51e251836cee5ffffaf403250
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2023-07-15 16:32:01 +0900

    m

--- redux-middleware/src/modules/sample.js
+++ redux-middleware/src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from "redux-actions";
 import * as api from "../lib/api";
+import createRequestThunk from "../lib/createRequestThunk";
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
@@ -9,41 +10,8 @@ const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
 const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
-export const getPost = id => async dispatch => {
-    dispatch({ type: GET_POST }); // 요청을 시작한 것을 알림.
-    try {
-        const response = await api.getPost(id);
-        dispatch({
-            type: GET_POST_SUCCESS,
-            payload: response.data
-        }); // 요청 성공
-    } catch (e) {
-        dispatch({
-            type: GET_POST_FAILURE,
-            payload: e,
-            error: true
-        });
-        throw e;
-    }
-};
-
-export const getUsers = id => async dispatch => {
-    dispatch({ type: GET_USERS }); // 요청을 시작한 것을 알림.
-    try {
-        const response = await api.getUsers(id);
-        dispatch({
-            type: GET_USERS_SUCCESS,
-            payload: response.data
-        }); // 요청 성공
-    } catch (e) {
-        dispatch({
-            type: GET_USERS_FAILURE,
-            payload: e,
-            error: true
-        });
-        throw e;
-    }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 const initialState = {
     loading: {
